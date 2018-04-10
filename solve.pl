178bf7b85164af1c95d4ef60e7ff56aeb1256a15
Ianpougher <ianpougher@gmail.com>
2018-04-10 14:27:32 -0600

    starts work on the maze solve algorithm

--- solve.pl
+++ solve.pl
@@ -4,6 +4,12 @@
 
 % Try a move in an "Up" direction, assumes Row and Column are bound.
 try(Row, Column, NextRow, NextColumn) :- NextRow is Row, NextColumn is Column - 1.
+% Try a move in an "down" direction, assumes Row and Column are bound.
+try(Row, Column, NextRow, NextColumn) :- NextRow is Row, NextColumn is Column + 1.
+% Try a move in an "left" direction, assumes Row and Column are bound.
+try(Row, Column, NextRow, NextColumn) :- NextRow is Row - 1, NextColumn is Column.
+% Try a move in an "right" direction, assumes Row and Column are bound.
+try(Row, Column, NextRow, NextColumn) :- NextRow is Row + 1, NextColumn is Column.
 
 % move(Maze, List, NewList, Row, Column, GoalRow, GoalColumn) - moves, 
 %   and keep on moving until the GoalRow and GoalColumn coordinates 
@@ -19,8 +25,11 @@ try(Row, Column, NextRow, NextColumn) :- NextRow is Row, NextColumn is Column - 
 %
 %   Print a barrier.
 % prints the top and bottom of the the maze, a helper function for the print maze function
+%solve(Maze) :- 
+
 printList(List):-
 printerate(List).
+
 printBox(_width):-
 write('+'),
 forall(between(1,_width,_),
@@ -50,9 +59,9 @@ checkCell(Maze,List,Row,Column) :- iterate(List,Row,Column).
 printerate([]).
 printerate([H|T]):-writeln(H),printerate(T).
 iterate([],Row,Column).
-iterate([H|T],Row,Column):- check(H,Row,Column),iterate(T).
+iterate([H|T],Row,Column) :- check(H,Row,Column),iterate(T).
 check([],Row,Column).
-check(H,Row,Column):-member(H,(Row,Column)),printcell(Maze,trail,Row,Column).
+check(H,Row,Column) :-member([Row,Column],H), write('*').
 
 %printMaze(Maze, List) ;- true.
 
